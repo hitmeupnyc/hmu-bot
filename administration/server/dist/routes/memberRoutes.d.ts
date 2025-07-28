968563441d9f053a455cd25dc091d2a4978a256f
Carl Vitullo <carl.vitullo@gmail.com>
2025-07-28 01:49:00 -0400

    Remove dist files

--- administration/server/dist/routes/memberRoutes.d.ts
+++
@@ -1,3 +0,0 @@
-declare const router: import("express-serve-static-core").Router;
-export { router as memberRoutes };
-//# sourceMappingURL=memberRoutes.d.ts.map
--- administration/server/dist/routes/webhookRoutes.d.ts
+++
@@ -1,3 +0,0 @@
-declare const router: import("express-serve-static-core").Router;
-export { router as webhookRoutes };
-//# sourceMappingURL=webhookRoutes.d.ts.map
